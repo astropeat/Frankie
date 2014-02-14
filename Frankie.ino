f18e4d5732eac67367c758317170387bdf14b1e2
trainman419 <legotown@aol.com>
2014-02-13 22:52:45 -0800

    Add compass and bluetooth for Due

--- Frankie.ino
+++ Frankie.ino
@@ -1,26 +1,31 @@
 #include <Protocol.h>
 #include <Servo.h>
+#include <Wire.h>
 
-Protocol link(Serial); // create a protocol object called 'link'
+Protocol btooth(Serial1); // create a protocol object called 'link'
 
 Servo motor;
 Servo steering;
 
+int HMC6352Address = 0x42;
+int slaveAddress;
 int led = 13;
+boolean ledState = false;
+byte headingData[2];
+int i, headingValue;
 
 #define MOTOR_NEUTRAL 92
 #define STEER_NEUTRAL 88
 
 void linkCallback(Packet &p) {
-  switch((char)p.type) {
+  switch(p.type) {
     case 'V': 
     //Speed & Direction - Vector
       {
-        digitalWrite(led, HIGH);
         int8_t spee = p.reads8();
         int8_t dir = p.reads8();
         motor.write( spee + MOTOR_NEUTRAL );
-        steering.write( dir + STEER_NEUTRAL ); 
+        steering.write( dir + STEER_NEUTRAL );
       }
       break;
       /*
@@ -53,18 +58,51 @@ void linkCallback(Packet &p) {
 }
 
 void setup() {
-  link.setCallback(linkCallback);
-  motor.attach(11);
-  steering.attach(10);
-  Serial.begin(57600);
+  // Attach bluetooth, motor, steering, serial
+  btooth.setCallback(linkCallback);
+  motor.attach(3);
+  steering.attach(2);
+  Serial1.begin(57600);
+    
+  // Attach compass!
+  // Shift the device's documented slave address (0x42) 1 bit right
+  // This compensates for how the TWI library only wants the
+  // 7 most significant bits (with the high bit padded with 0)
+  slaveAddress = HMC6352Address >> 1;   // This results in 0x21 as the address to pass to TWI
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);      // Set the LED pin as output
+  Wire.begin();
   
-  pinMode(led, OUTPUT);
   digitalWrite(led, HIGH);
   delay(500);
   digitalWrite(led, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  link.poll();
+  // Bluetooth
+  btooth.poll();
+  
+  //Compass
+  Wire.beginTransmission(slaveAddress);
+  Wire.write("A");              // The "Get Data" command
+  Wire.endTransmission();
+  delay(10);                   // The HMC6352 needs at least a 70us (microsecond) delay
+  // after this command.  Using 10ms just makes it safe
+  // Read the 2 heading bytes, MSB first
+  // The resulting 16bit word is the compass heading in 10th's of a degree
+  // For example: a heading of 1345 would be 134.5 degrees
+  Wire.requestFrom(slaveAddress, 2);        // Request the 2 byte heading (MSB comes first)
+  i = 0;
+  while(Wire.available() && i < 2)
+  { 
+    headingData[i] = Wire.read();
+    i++;
+  }
+  headingValue = headingData[0]*256 + headingData[1];  // Put the MSB and LSB together
+  Serial.print("Current heading: ");
+  Serial.print(int (headingValue / 10));     // The whole number part of the heading
+  Serial.print(".");
+  Serial.print(int (headingValue % 10));     // The fractional part of the heading
+  Serial.println(" degrees");
+  delay(500);
 }
